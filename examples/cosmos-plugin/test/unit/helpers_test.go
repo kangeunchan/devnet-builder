b2bfc07273ee15de7c4f79b06a896f0e56e720b5
kangeunchan <kangeunchan080310@gmail.com>
2026-02-19 14:26:47 +0900

    test(examples/cosmos-plugin): add helper unit tests
    Signed-off-by: kangeunchan <kangeunchan080310@gmail.com>

---
+++ examples/cosmos-plugin/test/unit/helpers_test.go
@@ -0,0 +1,25 @@
+package unit
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func asMap(v any) (map[string]any, bool) {
+	m, ok := v.(map[string]any)
+	return m, ok
+}
+
+func asSlice(v any) ([]any, bool) {
+	s, ok := v.([]any)
+	return s, ok
+}
+
+func mustMarshalJSON(t *testing.T, v any) []byte {
+	t.Helper()
+	b, err := json.Marshal(v)
+	if err != nil {
+		t.Fatalf("failed to marshal json: %v", err)
+	}
+	return b
+}
